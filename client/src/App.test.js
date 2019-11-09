8d6d7e75bf9ec6660489d1b81a07b74adf1efb92
William M <wmooney1984@gmail.com>
2019-11-08 22:44:12 -0500

    Added Tests - MVP Completed

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Testing Renders
+test('Card renders without crashing', () => {
+  render(<Card />);
+});
+
+test('NavBar renders without crashing', () => {
+  render(<NavBar />);
+});
+
+test('App renders without crashing', () => {
+  render(<App />);
 });
